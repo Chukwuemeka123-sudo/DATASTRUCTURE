3acaf8b4a0cc5403768d865dda35d62aec205093
chukwuemeka richard <132878535+Chukwuemeka123-sudo@users.noreply.github.com>
2024-06-11 15:00:59 +0100

    Add files via upload

---
+++ Mains.java
@@ -0,0 +1,13 @@
+public class Mains {
+    public static void main(String[] args) {
+        stack stack = new stack(capacity: 10);
+        int item;
+        stack.push(item: 45);
+        stack.push(item: 5);
+        stack.push(item: 15);
+        stack.push(item: 35);
+        stack.pop();
+        stack.pop();
+        stack.pop();
+    }
+}
